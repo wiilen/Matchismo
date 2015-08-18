0b1ac77cbc1e06a956d516f53f82a5f7b8e403a3
wiilen <Wiilen.png@gmail.com>
2015-08-18 19:27:06 +0800

    update setCards

---
+++ Matchismo/Model/SetCardDeck.m
@@ -0,0 +1,13 @@
+//
+//  SetCardDeck.m
+//  Matchismo
+//
+//  Created by LinWiilen on 15/8/11.
+//  Copyright (c) 2015年 WiilenLin. All rights reserved.
+//
+
+#import "SetCardDeck.h"
+
+@implementation SetCardDeck
+
+@end
---
+++ Matchismo/SetCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetCardViewController.h
+//  Matchismo
+//
+//  Created by LinWiilen on 15/8/11.
+//  Copyright (c) 2015年 WiilenLin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SetCardViewController : ViewController
+
+@end
